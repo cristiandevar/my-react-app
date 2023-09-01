1ea0947a4f9bf8cfe9c2adebcb1c3d714be1d4d0
cristiandevar <cristianprogramadorunsa@gmail.com>
2023-09-01 11:27:11 -0300

    -

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Test from './Examples/Test'
+import reportWebVitals from './reportWebVitals';
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import './index.css';
+import App from './App';
+import Test from './Examples/Test';
 
 // const myFirstElement = <h1> Hello React </h1>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
