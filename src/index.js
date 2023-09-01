392a4bcd26f5ec2ef63b2e5e22b8b67cd62d3dc5
cristiandevar <cristianprogramadorunsa@gmail.com>
2023-09-01 11:24:55 -0300

    Extra Changes

--- src/index.js
+++ src/index.js
@@ -1,51 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import RenderExample1 from './RenderExamples/RenderExample1';
-import ConditionalExample1 from './ConditionalExamples/ConditionalExample1';
-import ConditionalExample2 from './ConditionalExample2';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ListExample1 from './ListExamples/ListExample1';
-import FormsExample1 from './FormsExamples/FormsExample1';
-import FormsExample2 from './FormsExamples/FormsExample2';
-import FormsExample3 from './FormsExamples/FormsExample3';
-import FormsExample4 from './FormsExamples/FormsExample4';
-import FormsExample5 from './FormsExamples/FormsExample5';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Test from './Examples/Test'
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 // const myFirstElement = <h1> Hello React </h1>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const myCars = ["ford", "chevro"];
 // root.render(myFirstElement);
 root.render(
       <React.StrictMode>
-        <div>
-          <RenderExample1 />
-        </div>
-        <div>
-          <ConditionalExample1 cars = {myCars}/>
-        </div>
-        <div>
-          <ConditionalExample2 isGoal = {true}/>
-        </div>
-        <div>
-          <ListExample1 />
-        </div>
-        <div>
-          <FormsExample1 />
-        </div>
-        <div>
-          <FormsExample2 />
-        </div>
-        <div>
-          <FormsExample3 />
-        </div>
-        <div>
-          <FormsExample4 />
-        </div>
-        <div>
-          <FormsExample5 />
-        </div>
+        <App />
+        <Test />
       </React.StrictMode>
 );
 
